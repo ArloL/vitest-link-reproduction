63e2573ed8654443fdb04b5c7dad6d17aad0b955
Arlo O'Keeffe <tiger@k5d.de>
2023-08-30 09:07:46 +0200

    add resolve.dedupe

--- test-projects/vite-project/vite.config.js
+++ test-projects/vite-project/vite.config.js
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    dedupe: [
+      'react',
+      'react-dom',
+      'react/jsx-runtime',
+    ]
+  },
   test: {
     environment: 'jsdom',
   },
