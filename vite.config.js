7c1ce16c72dc494a3a88663bb02cdae00f9c50ae
Arlo O'Keeffe <tiger@k5d.de>
2025-09-02 12:39:33 +0200

    use with

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import path from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-import pkg from './package.json' assert { type: 'json' };
+import pkg from './package.json' with { type: 'json' };
 
 // https://vitejs.dev/config/
 export default defineConfig({
